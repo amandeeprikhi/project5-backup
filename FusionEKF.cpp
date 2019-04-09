4fa514dfca1ea664032e7ea4b2946001856490e8
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-04-09 17:53:27 +0530

    fixing error

--- FusionEKF.cpp
+++ FusionEKF.cpp
@@ -154,7 +154,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-    Hj_ tools.CalculateJacobian(ekf_.x_);
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
