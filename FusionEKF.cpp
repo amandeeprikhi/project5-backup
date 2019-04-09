c15ca25aeda9005e42131235cf8ee3e57f6a70bd
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-04-09 18:29:28 +0530

    removing the use of Hj_

--- FusionEKF.cpp
+++ FusionEKF.cpp
@@ -154,8 +154,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-    Hj_ = tools.CalculateJacobian(ekf_.x_);
-    ekf_.H_ = Hj_;
+//     Hj_ = tools.CalculateJacobian(ekf_.x_);
+//     ekf_.H_ = Hj_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
